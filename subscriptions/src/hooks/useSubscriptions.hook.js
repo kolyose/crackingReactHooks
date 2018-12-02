91186211c11e4d6ffc7aa9187dd3696b2f5e1456
kolyose <kolyose@gmail.com>
2018-12-02 22:50:30 +0200

    Refactor useSubscriptions hook

--- subscriptions/src/hooks/useSubscriptions.hook.js
+++ subscriptions/src/hooks/useSubscriptions.hook.js
@@ -1,33 +1,32 @@
-import { useState, useEffect } from 'react';
 import useComponentId from './useComponentId.hook';
 
 let subscriptionsByComponentId = {};
 
-export default (target) => {
-  const componentId = useComponentId();
-
-  if (!subscriptionsByComponentId) {
-    subscriptionsByComponentId[componentId] = {};
+const add = id => source => {
+  if (!source) return;
+  if (!subscriptionsByComponentId[id]) {
+    subscriptionsByComponentId[id] = new Set();
   }
 
-  let subscription = subscriptionsByComponentId[componentId][target];
-  if (!subscription) {
-    const [data, setData] = useState({});
-    const unsubscribe = () => {
-      delete subscriptionsByComponentId[componentId][target];
-    }
-  
-    subscription = [ data, unsubscribe ];
-    subscriptionsByComponentId[componentId][target] = subscription;  
-    
-    useEffect(() => {
-      // some real subscription here
-      setData({ ...subscriptionsByComponentId[componentId] });
-      return () => {
-        // some real unsubscription here
-      }
-    }, [ Object.keys[subscriptionsByComponentId[componentId]].length ]);   
+  subscriptionsByComponentId[id].add(source);
+};
+
+const remove = id => source => {
+  if (!source) return;
+  if (subscriptionsByComponentId[id]) {
+    subscriptionsByComponentId[id].delete(source);
   }
-    
-  return subscription; 
-}
+};
+
+const get = id => () =>
+  subscriptionsByComponentId[id] ? Array.from(subscriptionsByComponentId[id]).join(',') : '';
+
+export default () => {
+  const id = useComponentId();
+
+  return {
+    addSubscription: add(id),
+    removeSubscription: remove(id),
+    getSubscriptions: get(id),
+  };
+};
